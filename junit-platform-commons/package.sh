8c4ac59ad4458a182d6bff5dce2535f74e67886c
Oleg Tsal-Tsalko <oleg_tsal-tsalko@epuakyiw3274.kyiv.epam.com>
2017-07-10 17:38:13 +0300

    Fix exports in junit.platform.commons module

--- junit-platform-commons/package.sh
+++ junit-platform-commons/package.sh
@@ -7,6 +7,6 @@ mkdir -p mlib
 echo
 echo "*** Packaging junit.platform.commons module into modular JAR ***"
 ${JAVA_HOME}/bin/jar --create \
-    --file mlib/junit.platform.commons@1.0.0.jar \
+    --file ../mlib/junit.platform.commons@1.0.0.jar \
     --module-version 1.0.0 \
     -C mods/junit.platform.commons .
--- junit-platform-commons/src/main/java/module-info.java
+++ junit-platform-commons/src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module junit.platform.commons {
-    requires java.compiler;
+    requires java.compiler;     
     requires java.logging;
 
     exports org.junit.platform.commons;
+    exports org.junit.platform.commons.annotation;
+    exports org.junit.platform.commons.meta;
+    exports org.junit.platform.commons.support;
+    exports org.junit.platform.commons.util;
 }
