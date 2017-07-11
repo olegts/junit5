c3452634f00b2f46862da497717b2d3fb88d0af7
Oleg Tsal-Tsalko <oleg_tsal-tsalko@epuakyiw3274.kyiv.epam.com>
2017-07-11 15:03:18 +0300

    Modularize junit-jupiter-params module

---
+++ junit-jupiter-params/package.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export JAVA_HOME=/Users/oleg_tsal-tsalko/dev/jdk-9.jdk/Contents/Home
+
+echo
+echo "*** Packaging junit.jupiter.params module into modular JAR ***"
+${JAVA_HOME}/bin/jar --create \
+    --file ../mlib/junit.jupiter.params@5.0.0.jar \
+    --module-version 5.0.0 \
+    -C mods/junit.jupiter.params .
