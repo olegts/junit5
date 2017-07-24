ed72ab08eee0deb1a6af694f64b4a1043517d799
Oleg Tsal-Tsalko <oleg.tsalko@gmail.com>
2017-07-24 10:13:16 +0300

    Prepare runTests.sh template script

--- runTests.sh
+++ runTests.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+#!/*TODO: Set correct path to Intellij IDEA libs directories on your machine*/
 export IDEA_LIBS="/Applications/IntelliJ IDEA CE.app/Contents/lib"
 export IDEA_JUNIT_PLUGIN_LIBS="/Applications/IntelliJ IDEA CE.app/Contents/plugins/junit/lib"
 
@@ -7,19 +8,10 @@ export IDEA_JUNIT_PLUGIN_LIBS="/Applications/IntelliJ IDEA CE.app/Contents/plugi
 #org.junit.platform.commons.util.CollectionUtilsTests -> doesn't work
 echo "Run org.junit.mytests.SimpleTest by Intellij IDEA JUnit runner using module path"
 ${JAVA_HOME}/bin/java \
--p mlib:lib \
---add-modules junit.jupiter.engine,junit.jupiter.params,junit.platform.launcher \
--cp "${IDEA_LIBS}/idea_rt.jar:${IDEA_JUNIT_PLUGIN_LIBS}/junit-rt.jar:${IDEA_JUNIT_PLUGIN_LIBS}/junit5-rt.jar:platform-tests/build/classes/java/test:classpath/byte-buddy-agent-1.6.11.jar:classpath/byte-buddy-agent-1.6.11.jar" \
+#!/*TODO: Add missing bits*/
+-cp "${IDEA_LIBS}/idea_rt.jar:${IDEA_JUNIT_PLUGIN_LIBS}/junit-rt.jar:${IDEA_JUNIT_PLUGIN_LIBS}/junit5-rt.jar" \
 com.intellij.rt.execution.junit.JUnitStarter -ideVersion5 -junit5 org.junit.mytests.SimpleTest
 
 echo "Run org.junit.mytests.SimpleTest by org.junit.platform.console.ConsoleLauncher using module path"
 ${JAVA_HOME}/bin/java \
--p mlib:lib \
--cp "platform-tests/build/classes/java/test:classpath/byte-buddy-agent-1.6.11.jar:classpath/byte-buddy-agent-1.6.11.jar" \
--m junit.platform.console -c org.junit.mytests.SimpleTest
-
-#echo "Run org.junit.platform.commons.util.CollectionUtilsTests using classpath"
-#${JAVA_HOME}/bin/java \
-#-p lib \
-#-cp "${IDEA_LIBS}/idea_rt.jar:${IDEA_JUNIT_PLUGIN_LIBS}/junit-rt.jar:${IDEA_JUNIT_PLUGIN_LIBS}/junit5-rt.jar:platform-tests/build/classes/java/test:classpath/byte-buddy-agent-1.6.11.jar:classpath/byte-buddy-agent-1.6.11.jar:mlib/junit.platform.commons@1.0.0.jar:mlib/junit.platform.engine@1.0.0.jar:mlib/junit.platform.launcher@1.0.0.jar:mlib/junit.jupiter.api@5.0.0.jar:mlib/junit.jupiter.engine@5.0.0.jar:mlib/junit.jupiter.params@5.0.0.jar" \
-#com.intellij.rt.execution.junit.JUnitStarter -ideVersion5 -junit5 org.junit.platform.commons.util.CollectionUtilsTests
+#!/*TODO: Use junit.platform.console module to run your tests*/
